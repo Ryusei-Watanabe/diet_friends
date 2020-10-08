e097cde5cb4203a47ac2b018d4bee1e716aea6fc
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-10-09 02:05:39 +0900

    [add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   # get 'diaries/index'
   # get 'diaries/show'
   # get 'diaries/edit'
   # get 'diaries/new'
   resources :diaries
+  root "diaries#index"
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

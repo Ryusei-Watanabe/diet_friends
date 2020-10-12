96916c70701be5952f318a6dfcb3c5ad4bf7b95c
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-10-12 19:12:07 +0900

    [add]ransck_user_seaarch #6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show]
   def index
-    @users = User.all
+    @q = User.ransack(params[:q])
+    @users = @q.result(distinct: true)
   end
   def show
   end

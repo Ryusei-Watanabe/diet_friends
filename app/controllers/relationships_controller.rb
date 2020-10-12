729646683eaa87a37860d7985fcddc5e6eb40cf0
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-10-12 18:19:46 +0900

    [add]ajax_relationships #6

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,14 @@
+class RelationshipsController < ApplicationController
+  respond_to? :js
+  def create
+    if logged_in?
+      @user = User.find(params[:relationship][:followed_id])
+      current_user.follow!(@user)
+    end
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   devise_for :users, controllers: {
       registrations: 'users/registrations'
   }
   resources :users, only: [:index, :show]
   resources :diaries
+  resources :relationships, only: [:create, :destroy]
   root "users#show"
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end

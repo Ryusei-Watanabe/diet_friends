509202f363848ebead51de13c14e7a6cc57a1b6b
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-10-13 14:13:31 +0900

    [fixed]検索機能_turbolinks #6

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -2,13 +2,22 @@ class DiariesController < ApplicationController
   def index
     @diaries = Diary.all
   end
-
+  def new
+    @diary = Diary.new
+  end
+  def create
+    @diary = current_user.diaries.build
+  end
   def show
   end
 
   def edit
   end
-
-  def new
+  private
+  def set_diary
+    @diary = Diary.find(params[:id])
+  end
+  def diary_params
+    params.require(:diary).permit(:title, body_weight, study)
   end
 end

fc0b7f86e397e6e1ca0cc039af808d935de01b7e
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-10-13 15:32:39 +0900

    [add]diary&user_model_fixed

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -6,7 +6,16 @@ class DiariesController < ApplicationController
     @diary = Diary.new
   end
   def create
-    @diary = current_user.diaries.build
+    @diary = current_user.diaries.build(diary_params)
+    if params[:back]
+      format.html{render :new}
+    else
+      if @diary.save
+        redirect_to diaries_path, notice: 'Post was successfully created.'
+      else
+        render
+      end
+    end
   end
   def show
   end
@@ -18,6 +27,6 @@ class DiariesController < ApplicationController
     @diary = Diary.find(params[:id])
   end
   def diary_params
-    params.require(:diary).permit(:title, body_weight, study)
+    params.require(:diary).permit(:title, :body_weight, :study)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
+  has_many :diaries, dependent: :destroy
   has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed

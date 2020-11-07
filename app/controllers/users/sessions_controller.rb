bfff0f99644876d2454a2a9419d191da2486a3a2
Watanabe Ryusei <r8y3us1e8i@icloud.com>
2020-11-08 03:16:15 +0900

    [add]guest_user

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,6 +8,19 @@ class Users::SessionsController < Devise::SessionsController
     redirect_to :root if user_signed_in?
     super
   end
+  # def new_guest
+  #   user = User.guest
+  #   sign_in user
+  #   redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  # end
+  def new_guest
+    user = User.find(1)
+    user.update(email: 'guest@example.com',name: 'ゲストユーザー') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+    sign_in user
+    redirect_to user_path(user.id), notice: "ゲストユーザーでログインしました"
+  end
 
   # POST /resource/sign_in
   # def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -29,4 +29,10 @@ class User < ApplicationRecord
   def unfollow!(other_user)
     active_relationships.find_by(followed_id: other_user.id).destroy
   end
+  # def self.guest
+  #   find_or_create_by!(email: 'guest@example.com') do |user|
+  #     user.password = SecureRandom.urlsafe_base64
+  #     # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+  #   end
+  # end
 end
